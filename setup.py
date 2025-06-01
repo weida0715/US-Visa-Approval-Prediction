0e0d6013fba7f520818a10ddfd234704af0fecdb
weida0715 <ng62312@gmail.com>
2025-06-01 22:55:19 +0800

    Requirements added

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="us_visa",
+    version="0.0.1",
+    author="Wei Da",
+    author_email="ng62312@gmail.com",
+    packages=find_packages()
+)
