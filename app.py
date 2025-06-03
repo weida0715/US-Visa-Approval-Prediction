35b545242bb8d9809358e34adc76ade7fa8ff27d
weida0715 <ng62312@gmail.com>
2025-06-03 13:03:09 +0800

    Vercel

--- app.py
+++ app.py
@@ -117,5 +117,5 @@ async def predictRouteClient(request: Request):
         return {"status": False, "error": f"{e}"}
 
 
-# if __name__ == "__main__":
-#     app_run(app, host=APP_HOST, port=APP_PORT)
+if __name__ == "__main__":
+    app_run(app, host=APP_HOST, port=APP_PORT)
